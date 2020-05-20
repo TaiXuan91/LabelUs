0fa45a394893a092c70a6d53df59b5bb368270f7
wangqi <x913724685@live.com>
2020-05-20 18:58:45 +0800

    add image view function

--- LabelUs/MainWindow.xaml.cs
+++ LabelUs/MainWindow.xaml.cs
@@ -23,6 +23,41 @@ namespace LabelUs
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void openImage_Click(object sender, RoutedEventArgs e)
+        {
+            // Create Image Element
+            Image myImage = new Image();
+            myImage.Width = 600;
+
+            // Create source
+            BitmapImage myBitmapImage = new BitmapImage();
+
+            // BitmapImage.UriSource must be in a BeginInit/EndInit block
+            myBitmapImage.BeginInit();
+            myBitmapImage.UriSource = new Uri(@"C:\Users\Administrator.000\Pictures\总体直方图.png");
+
+            // To save significant application memory, set the DecodePixelWidth or
+            // DecodePixelHeight of the BitmapImage value of the image source to the desired
+            // height or width of the rendered image. If you don't do this, the application will
+            // cache the image as though it were rendered as its normal size rather then just
+            // the size that is displayed.
+            // Note: In order to preserve aspect ratio, set DecodePixelWidth
+            // or DecodePixelHeight but not both.
+            myBitmapImage.DecodePixelWidth = 600;
+            myBitmapImage.EndInit();
+            //set image source
+            myImage.Source = myBitmapImage;
+            this.imageBox.Source = myBitmapImage;
+        }
+
+        private void closeWindow_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
+
+//Source=
